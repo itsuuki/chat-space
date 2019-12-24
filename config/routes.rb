5ff947d7c421d20dcae293a5fb49be485fd98769
itsuuki <53384302+itsuuki@users.noreply.github.com>
2019-12-24 20:01:22 +0900

    group-chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   
 
+  
+  get 'groupsrails/g'
+
+  get 'groupsrails/controller'
+
+  get 'groupsrails/groups'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
+  root "groups#index"
 
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
