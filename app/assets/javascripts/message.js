4a8c32e689960c418a0fa238ae4dd75ad7989a0a
itsuuki <53384302+itsuuki@users.noreply.github.com>
2020-01-06 19:29:25 +0900

    ajax-chat2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -41,7 +41,6 @@ $(function(){
   }
   $("#new_message").on("submit", function(e){
     e.preventDefault()
-    // console.logを用いてイベント発火しているか確認
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
