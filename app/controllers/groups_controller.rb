67d3d6b6f1adf5428a1d50b3c54d1e3e49a621d7
itsuuki <53384302+itsuuki@users.noreply.github.com>
2020-01-08 13:14:09 +0900

    inkle-chat1

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-
+  before_action :set_group, only: [:edit, :update]
   def index
   end
   def new
@@ -15,19 +15,25 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+  
   def update
     if @group.update(group_params)
-      redirect_to group_path, notice: "グループを更新しました"
+      redirect_to root_path, notice: "グループを更新しました"
     else 
       render :edit
     end
   end
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [])
+    params.require(:group).permit(:name, {user_ids: []})
   end
 
   def set_group
-    @group = Group.find(params [:id])
+    # binding.pry
+    @group = Group.find(params[:id])
+    
   end
 end
