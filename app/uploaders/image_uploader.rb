a857622606147c1dc358980cad6bd099d13f7d7a
itsuuki <53384302+itsuuki@users.noreply.github.com>
2020-01-09 21:14:11 +0900

    gem-chat5

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -48,4 +48,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   process resize_to_fit: [800, 800]
+
+  
 end
+
